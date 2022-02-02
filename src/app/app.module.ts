410f9bc99276344cc18548c7e18330a731c77989
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-01 23:53:42 -0300

    Desenvolvimento da parte principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
